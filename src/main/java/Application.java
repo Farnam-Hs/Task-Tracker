84ce9e3b9b9a144b79ab4e446892788040c041ba
Farnam <farnam.hs@yahoo.com>
2024-08-23 17:16:47 +0330

    Changed Application to run the project

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,15 @@
+import cli.TaskManagerCLI;
+import repository.TaskRepositoryImpl;
+import service.TaskServiceImpl;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    private static final String PATH = "tasks.json";
+
+    public static void main(String[] args) throws IOException {
+        new TaskManagerCLI(new TaskServiceImpl(new TaskRepositoryImpl(Path.of(PATH)))).run();
     }
 }
